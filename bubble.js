4b30ecda1f970c1278cd438696fa23adbf333dde
Olivia Golden <olivia.golden.w@gmail.com>
2024-03-26 10:29:22 -0500

    bubble

--- bubble.js
+++ bubble.js
@@ -1,3 +1,14 @@
-function bubbleSort() {}
+function bubbleSort(array) {
+    for (let i = 0; i < array.length; i++) {
+        for (let j = 0; j < array.length - i; j++) {
+            if (array[j] > array[j + 1]) {
+                const temp = array[j];
+                array[j] = array[j + 1];
+                array[j + 1] = temp;
+            }
+        }
+    }
+    return array;
+}
 
 module.exports = bubbleSort;
