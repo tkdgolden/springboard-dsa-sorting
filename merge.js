80a48a5b23e98c38bdead0d5b1ed6e443836393a
Olivia Golden <olivia.golden.w@gmail.com>
2024-03-26 11:03:21 -0500

    merge

--- merge.js
+++ merge.js
@@ -1,5 +1,30 @@
-function merge() {}
+function merge(array1, array2) {
+    let i = 0;
+    let j = 0;
+    const newArray = [];
+    while (i < array1.length || j < array2.length) {
+        if (i >= array1.length) {
+            newArray.push(array2.slice(j));
+            return newArray.flat();
+        }
+        if (j >= array2.length) {
+            newArray.push(array1.slice(i));
+            return newArray.flat();
+        }
+        if (array1[i] < array2[j]) {
+            newArray.push(array1[i]);
+            i++;
+        }
+        else {
+            newArray.push(array2[j]);
+            j++;
+        }
+    }
+    return newArray;
+}
 
-function mergeSort() {}
+function mergeSort() {
+    
+}
 
 module.exports = { merge, mergeSort};
