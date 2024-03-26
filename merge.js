dff59f14f1e7d2ac6a9337354704a44b8d740286
Olivia Golden <olivia.golden.w@gmail.com>
2024-03-26 11:10:57 -0500

    merge sort

--- merge.js
+++ merge.js
@@ -23,8 +23,13 @@ function merge(array1, array2) {
     return newArray;
 }
 
-function mergeSort() {
-    
+function mergeSort(array) {
+    if (array.length <= 1) return array;
+
+    const mid = Math.floor(array.length/2);
+    const left = mergeSort(array.slice(0, mid));
+    const right = mergeSort(array.slice(mid));
+    return merge(left, right);
 }
 
 module.exports = { merge, mergeSort};
